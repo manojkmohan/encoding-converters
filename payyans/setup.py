1c8368b19b6a51b4252a8651c4989ed7afc8a789
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2013-06-01 16:11:34 +0530

    pep8 formatting

--- payyans/setup.py
+++ payyans/setup.py
@@ -6,14 +6,15 @@ doclines = __doc__.split("\n")
 setup(name='payyans',
       version='0.7',
       description=doclines[0],
-      long_description = "\n".join(doclines[:]),
-      platforms = ["Linux"],
+      long_description="\n".join(doclines[:]),
+      platforms=["Linux"],
       author='Santhosh Thottingal, Nishan Naseer, Rajeesh K Nambiar',
       author_email='santhosh.thottingal@gmail.com , nishan.naseer@gmail.com, rajeeshknambiar@gmail.com',
       url='http://smc.org.in/Payyans',
-      license = 'http://www.gnu.org/copyleft/gpl.html',
+      license='http://www.gnu.org/copyleft/gpl.html',
       packages=['payyans'],
-      data_files=[('/usr/share/payyans/maps',['maps/karthika.map','maps/indulekha.map','maps/revathi.map']),
-		   ('/usr/bin',['payyans/payyans']),
-	       ('/usr/share/doc/payyans-0.7',['docs/README','docs/LICENSE','docs/ChangeLog'])]
+      data_files=[(
+          '/usr/share/payyans/maps', ['maps/karthika.map', 'maps/indulekha.map', 'maps/revathi.map']),
+     ('/usr/bin', ['payyans/payyans']),
+          ('/usr/share/doc/payyans-0.7', ['docs/README', 'docs/LICENSE', 'docs/ChangeLog'])]
       )
